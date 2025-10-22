47d704e0b91a6f973f7fa8b20e32d20e6b518004
Aakash Rajlawat <aakashsky472@gmail.com>
2025-10-22 16:29:55 +0545

    connected to mongodb

---
+++ connect.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+
+
+const connectionToMongoDb = async () => {
+
+    await mongoose.connect('mongodb://127.0.0.1:27017/toDoList')
+    .then(()=> {console.log('Mongodb connected successfully.')})
+    .catch((err) => {console.log(`The Mongodb was unable to connect due to the following err :\n ${err}`)});
+
+};
+
+module.exports = connectionToMongoDb;
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require ('express');
 const app = express();
 const PORT = 8001;
+const connectionToMongoDb = require('./connect')
 
-
+connectionToMongoDb();
 
 app.listen(PORT,()=>{
     console.log(`The server is successfully running on port ${PORT}`);
